4965dce4bebf2affe106ca8ba71619c000612f66
Jayachandra36 <Jayravan36@gmail.com>
2021-07-12 19:36:54 +0530

    set static root

--- myquiz/settings.py
+++ myquiz/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR +  "/static/",
 ]
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
